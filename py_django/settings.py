16a9b091a5747e3f07e5d09cb611c0a69efd363f
João Francisco <jofrancb@users.noreply.github.com>
2022-02-27 13:58:30 -0300

    Configurando a SECRET_KEY com decouple.

--- py_django/settings.py
+++ py_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j*sdwq4@)q(hhr83ylcm5&^=b)%165#md@i!j#*$dse)1fwn=q'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default = False)
